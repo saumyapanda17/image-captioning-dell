ef0b2c7449ec48abfbd202d757d9f95e104cce3b
saswatsam786 <saswat.sam786@gmail.com>
2022-11-10 00:52:51 +0530

    added object-detection tenserflow

--- Frontend2/image-caption/src/App.js
+++ Frontend2/image-caption/src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./Screen/HomeScreen";
 import Upload from "./Screen/Upload";
 import Navbar from "./Components/Navbar";
+import Object from "./Screen/Object";
 function App() {
   return (
     <div className="appContainer">
@@ -11,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/upload" element={<Upload />} />
+          <Route path="/objectDetection" element={<Object />} />
           <Route path="/" element={<HomeScreen />} />
         </Routes>
       </BrowserRouter>
--- Frontend2/image-caption/src/Screen/HomeScreen.jsx
+++ Frontend2/image-caption/src/Screen/HomeScreen.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import homeImage from '../images/home3.jpg';
-import './HomeScreen.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import homeImage from "../images/home3.jpg";
+import "./HomeScreen.css";
 const HomeScreen = () => {
-    return (
-        <div style={{ backgroundImage: `url(${homeImage})`, minHeight: "95vh" }}
-            className="homeDiv">
-            <div className='homeInside'>
-                <Link>
-                    <button>Object Detection</button>
-                </Link>
-                <br />
-                <br />
-                <Link to='/upload'>
-                    <button>Image Captioning</button>
-                </Link>
-            </div>
+  return (
+    <div
+      style={{ backgroundImage: `url(${homeImage})`, minHeight: "95vh" }}
+      className="homeDiv"
+    >
+      <div className="homeInside">
+        <Link to="/objectDetection">
+          <button>Object Detection</button>
+        </Link>
+        <br />
+        <br />
+        <Link to="/upload">
+          <button>Image Captioning</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-
-        </div>
-    )
-}
-
-export default HomeScreen
+export default HomeScreen;
